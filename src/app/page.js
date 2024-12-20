ee67bf7843365794d13f1cbf4745bffc16eea9c4
Donald La <donaldla7@gmail.com>
2024-12-20 13:41:31 +1100

    Change startup link to Heffron.ai

--- src/app/page.js
+++ src/app/page.js
@@ -18,12 +18,12 @@ export default function Home() {
     >
       <ShaderGradient
         control='query'
-        urlString='https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23ff08ea&bgColor2=%2300b83a&brightness=1.2&cAzimuthAngle=250&cDistance=1.5&cPolarAngle=140&cameraZoom=12.5&color1=%2300400d&color2=%23005252&color3=%2300ebeb&embedMode=off&envPreset=dawn&fov=45&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=2.2&positionX=0&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.5&rotationX=0&rotationY=0&rotationZ=140&shader=defaults&type=sphere&uAmplitude=2.6&uDensity=0.7&uFrequency=5.5&uSpeed=0.8&uStrength=0.1&uTime=0&wireframe=false'> </ShaderGradient>
+        urlString='https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23ff08ea&bgColor2=%2300b83a&brightness=1.2&cAzimuthAngle=250&cDistance=1.5&cPolarAngle=140&cameraZoom=5&color1=%2300400d&color2=%23005252&color3=%2300ebeb&embedMode=off&envPreset=dawn&fov=45&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=2.2&positionX=0&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.5&rotationX=0&rotationY=0&rotationZ=140&shader=defaults&toggleAxis=false&type=sphere&uAmplitude=2.6&uDensity=0.7&uFrequency=5.5&uSpeed=0.8&uStrength=0.1&uTime=0&wireframe=false&zoomOut=false'> </ShaderGradient>
       </ShaderGradientCanvas>
       <div className="fixed top-0 left-0 p-4 flex items-end w-full flex-col gap-2">
           <div className ="flex justify-end gap-2 flex-wrap">
-          <a href = "https://quackedai.com" target = "_blank">
-            Quacked AI
+          <a href = "https://heffron.ai" target = "_blank">
+            Heffron.ai
           </a>
 
           <a href = "https://github.com/quackeducky" target = "_blank">
@@ -40,7 +40,7 @@ export default function Home() {
         </div>
      </div>
 
-    <div className="fixed bottom-0 p-4 flex items-end justify-between w-full">
+    <div className="fixed bottom-0 p-4 flex items-end justify-between w-full ">
         <h1> Donald La </h1>
 
         <div className="flex flex-col items-end gap-2"> 
